ab8488e8c2be3c66abaed842ecfcdf733901cead
Spandan Barve <spandan567@gmail.com>
2025-09-18 03:54:12 +0800

    forking not forking working

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,36 +6,36 @@ const config: HardhatUserConfig = {
   paths: {
     sources: "./src",
   },
-  networks: {
-    hardhat: {
-      chainId: 314159,
-      forking: {
-        url: "https://api.calibration.node.glif.io/rpc/v1",
-      },
-      chains: {
-        "314159": {
-          hardforkHistory: {
-            // frontier: 3026000,
-            // homestead: 3026000,
-            // dao: 3026000,
-            // tangerineWhistle: 3026000,
-            // spuriousDragon: 3026000,
-            // byzantium: 3026000,
-            // constantinople: 3026000,
-            // petersburg: 3026000,
-            // istanbul: 3026000,
-            // muirGlacier: 3026000,
-            // berlin: 3026000,
-            london: 302600,
-            // arrowGlacier: 3026000,
-            // grayGlacier: 3026000,
-            // paris: 3026000,
-          },
-        },
-      },
-      gas: "auto",
-    },
-  },
+  // networks: {
+  //   hardhat: {
+  //     chainId: 314159,
+  //     forking: {
+  //       url: "https://api.calibration.node.glif.io/rpc/v1",
+  //     },
+  //     chains: {
+  //       "314159": {
+  //         hardforkHistory: {
+  //           // frontier: 3026000,
+  //           // homestead: 3026000,
+  //           // dao: 3026000,
+  //           // tangerineWhistle: 3026000,
+  //           // spuriousDragon: 3026000,
+  //           // byzantium: 3026000,
+  //           // constantinople: 3026000,
+  //           // petersburg: 3026000,
+  //           // istanbul: 3026000,
+  //           // muirGlacier: 3026000,
+  //           // berlin: 3026000,
+  //           london: 302600,
+  //           // arrowGlacier: 3026000,
+  //           // grayGlacier: 3026000,
+  //           // paris: 3026000,
+  //         },
+  //       },
+  //     },
+  //     gas: "auto",
+  //   },
+  // },
 };
 
 export default config;
