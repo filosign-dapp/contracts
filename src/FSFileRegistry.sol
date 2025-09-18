f3fcc91aca79ced484079685666cde8f156d95e3
Spandan Barve <spandan567@gmail.com>
2025-09-19 04:15:37 +0800

    remove signature verification from file registry as tx has enough information alreat

--- src/FSFileRegistry.sol
+++ src/FSFileRegistry.sol
@@ -1,17 +1,15 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.26;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "./interfaces/IFSManager.sol";
 
 contract FSFileRegistry {
-    using ECDSA for bytes32;
-
     struct FileData {
-        string cid;
+        bytes32 pieceCidPrefix;
+        uint16 pieceCidTail;
         address sender;
         address recipient;
         bool acked;
-        bytes32 ownerSignature;
     }
 
     struct SignatureData {
@@ -23,14 +21,46 @@ contract FSFileRegistry {
         bytes32 s;
     }
 
-    function verifySignature(
-        address signer,
-        bytes32 messageHash,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) internal pure returns (bool) {
-        address recovered = ECDSA.recover(messageHash, v, r, s);
-        return recovered == signer;
+    mapping(bytes32 => FileData) private _files;
+    mapping(bytes32 => SignatureData) private _signatures;
+
+    IFSManager public manager;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function acknowledge(bytes32 cidIdentifier_) external {
+        FileData storage file = _files[cidIdentifier_];
+        require(file.pieceCidPrefix != bytes32(0), "File not registered");
+        require(file.recipient == msg.sender, "Only recipient can ack");
+        require(file.acked == false, "Already acknowledged");
+
+        file.acked = true;
+    }
+
+    function registerFile(
+        bytes32 pieceCidPrefix_,
+        uint16 pieceCidTail_,
+        address recipient_
+    ) external {}
+
+    function cidIdentifier(
+        bytes32 pieceCidPrefix_,
+        uint16 pieceCidTail_
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(pieceCidPrefix_, pieceCidTail_));
+    }
+
+    function getFileData(
+        bytes32 cidIdentifier_
+    ) external view returns (FileData memory) {
+        return _files[cidIdentifier_];
+    }
+
+    function getSignatureData(
+        bytes32 cidIdentifier_
+    ) external view returns (SignatureData memory) {
+        return _signatures[cidIdentifier_];
     }
 }
