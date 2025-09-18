2a8c037a73ccd8664958eff9344ab4b3a6224810
Spandan Barve <spandan567@gmail.com>
2025-09-19 04:57:09 +0800

    util to parse pieceid

---
+++ services/utils.ts
@@ -0,0 +1,37 @@
+import { CID } from "multiformats/cid";
+import { toHex } from "viem";
+
+function parsePieceCid(pieceCid: string) {
+  const cid = CID.parse(pieceCid);
+
+  const codecNumeric = cid.code;
+  const mh = cid.multihash;
+  const multihashCode = mh.code;
+  const rawDigest = mh.digest;
+  const digestLength = rawDigest.length;
+
+  if (digestLength < 2) throw new Error("digest too short (<2 bytes)");
+
+  const prefix = new Uint8Array(32);
+
+  prefix.set(rawDigest.subarray(0, Math.min(32, digestLength)));
+
+  const b1 = rawDigest[digestLength - 2];
+  const b2 = rawDigest[digestLength - 1];
+  const digestTail = (b1 << 8) | b2;
+
+  return {
+    multihashCode,
+    codecNumeric,
+    digestPrefix: toHex(prefix),
+    digestTail,
+    digestLength,
+    rawDigest,
+  };
+}
+
+console.log(
+  parsePieceCid(
+    "bafkzcibcbub2cd46abwvhoohwhmmjugyjibda32vn4a4qlcrv5dc76s24s67qai"
+  )
+);
