393503234914e4b0484e3a099d4d093ad1b67acb
Spandan Barve <spandan567@gmail.com>
2025-09-22 20:49:46 +0800

    new build

--- dist/hardhat.config.d.ts
+++
@@ -1,4 +0,0 @@
-import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
-declare const config: HardhatUserConfig;
-export default config;
--- dist/ignition/modules/Lock.d.ts
+++
@@ -1,4 +0,0 @@
-declare const LockModule: import("@nomicfoundation/ignition-core").IgnitionModule<"LockModule", string, {
-    lock: import("@nomicfoundation/ignition-core").NamedArtifactContractDeploymentFuture<"Lock">;
-}>;
-export default LockModule;
